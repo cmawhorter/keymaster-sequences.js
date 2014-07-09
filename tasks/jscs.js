ec852dc5a592ef9b962a65d213d3b61b97ae9b7e
Cory Mawhorter <cory.mawhorter@gmail.com>
2014-07-08 22:01:08 -0700

    jshint and demo

--- tasks/jscs.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  options: {
-    preset: 'google',
-    config: '.jscs.json'
-  },
-  lib: ['lib/<%= pkg.name %>.js'],
-  test: ['test/spec/{,*/}*.js'],
-  gruntfile: ['Gruntfile.js', 'tasks/{,*/}*.js']
-};
